c258e89c78f65e94ca8089f03f3f08d9e7f8291c
Maxime LESBROS <maxime.lesbros@rolex.com>
2025-12-30 12:29:12 +0100

    fix: keystatic redirect

--- src/app/api/keystatic/[...params]/route.ts
+++ src/app/api/keystatic/[...params]/route.ts
@@ -5,12 +5,29 @@ import { loadSecrets } from '@/lib/secrets';
 
 export const dynamic = 'force-dynamic';
 
+function rewriteUrl(request: Request) {
+    const forwardedHost = request.headers.get("x-forwarded-host");
+    const forwardedProto = request.headers.get("x-forwarded-proto");
+
+    if (forwardedHost && forwardedProto) {
+        const url = new URL(request.url);
+
+        url.hostname = forwardedHost;
+        url.protocol = forwardedProto;
+        url.port = "";
+
+        return new Request(url, request);
+    }
+
+    return request;
+}
+
 export async function GET(request: Request) {
   await loadSecrets();
   const { GET: _GET } = makeRouteHandler({
     config: keystaticConfig,
   });
-  return _GET(request);
+  return _GET(rewriteUrl(request));
 }
 
 export async function POST(request: Request) {
@@ -18,5 +35,5 @@ export async function POST(request: Request) {
   const { POST: _POST } = makeRouteHandler({
     config: keystaticConfig,
   });
-  return _POST(request);
+  return _POST(rewriteUrl(request));
 }
